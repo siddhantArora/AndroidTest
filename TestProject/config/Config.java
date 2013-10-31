392c84b676fcdf4a14831ffd41e1c4c29f20a0fd
Siddhant.Arora <Siddhanta@INHLSIDDHANTA.COMAKEIT.COM>
2013-10-31 12:57:08 +0530

    Test Project

---
+++ TestProject/config/Config.java
@@ -0,0 +1,7 @@
+package com.sid.advsdk;
+
+public class Config
+{
+	
+	public final static boolean SDK_TYPE = @CONFIG.SDK_TYPE@;
+}
---
+++ TestProject/src/com/sid/advsdk/test/Config.java
@@ -0,0 +1,9 @@
+package com.sid.advsdk.test;
+
+public class Config
+{
+	/** Whether to create SALE or LEAD SDK Jar. */
+	/** SDK_TYPE =TRUE ==>SALE SDK */
+	/** SDK_TYPE =FALSE ==>LEAD SDK */
+	public final static boolean SDK_TYPE = true;
+}
