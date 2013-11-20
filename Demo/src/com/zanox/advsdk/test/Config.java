e369680539f7bb38975da43d90819bbe4e1a169b
siddhart arora <siddhart@INHYLSIDDARTA.(none)>
2013-11-20 16:47:11 +0530

    AlexDemo

---
+++ Demo/src/com/zanox/advsdk/test/Config.java
@@ -0,0 +1,9 @@
+package com.zanox.advsdk.test;
+
+public class Config
+{
+	/** Whether to create SALE or LEAD SDK Jar. */
+	/** SDK_TYPE =TRUE ==>SALE SDK */
+	/** SDK_TYPE =FALSE ==>LEAD SDK */
+	public final static boolean SDK_TYPE = true;
+}
