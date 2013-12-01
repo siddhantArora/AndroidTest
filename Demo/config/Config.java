6ce7b5ddc787bf2863793c7cfab519a501fe976c
siddhart arora <siddhart@INHYLSIDDARTA.(none)>
2013-12-02 01:29:23 +0530

    removed

--- Demo/config/Config.java
+++
@@ -1,7 +0,0 @@
-package com.zanox.advsdk;
-
-public class Config
-{
-	/** Whether or not to include logging statements in the application. */
-	public final static boolean SDK_TYPE = @CONFIG.SDK_TYPE@;
-}
