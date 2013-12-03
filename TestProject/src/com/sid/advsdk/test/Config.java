6371a63a83adb0cb2dbf6508fe0aca06bbc6bd3d
siddhart arora <siddhart@INHYLSIDDARTA.(none)>
2013-12-03 15:43:27 +0530

    Deleted

--- TestProject/src/com/sid/advsdk/test/Config.java
+++
@@ -1,9 +0,0 @@
-package com.sid.advsdk.test;
-
-public class Config
-{
-	/** Whether to create SALE or LEAD SDK Jar. */
-	/** SDK_TYPE =TRUE ==>SALE SDK */
-	/** SDK_TYPE =FALSE ==>LEAD SDK */
-	public final static boolean SDK_TYPE = true;
-}
